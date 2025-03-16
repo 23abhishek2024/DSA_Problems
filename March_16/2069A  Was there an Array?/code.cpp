2cae5bdf2014bf92aa5057f87a28fc511e11c16e
23abhishek2024 <abhishek78579@gmail.com>
2025-03-16 22:05:42 +0530

    Update code.cpp

--- March_16/2069A  Was there an Array?/code.cpp
+++ March_16/2069A  Was there an Array?/code.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int b[n-2];
+        for(int i=0; i<n-2; i++){
+            cin>>b[i];
+        }
+        bool result = false;
+        for(int i=1; i<n-3; i++){
+            if(b[i-1]==1&&b[i]==0&&b[i+1]==1){
+                result=true;
+                break;
+            }
+        }
+        if(result){
+            cout<<"no"<<endl;
+        }
+        else{
+            cout<<"yes"<<endl;
+        }
+    }
+    return 0;
+}
