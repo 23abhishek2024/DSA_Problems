3a7f0cc8a3338a091a3ac0d38122de9fa030c54b
23abhishek2024 <abhishek78579@gmail.com>
2025-03-17 09:48:26 +0530

    Update code.cpp

--- March_17/7. Reverse Integer/code.cpp
+++ March_17/7. Reverse Integer/code.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int get_reverse(int n){
+        long long ans_=0;
+        while(n!=0){
+        int rem=n%10;
+        ans_=rem+ans_*10;
+        n=n/10;
+    }
+    if (ans_ < INT32_MIN || ans_ > INT32_MAX) return 0;
+    return (int)ans_;
+    }
+    int reverse(int x) {
+            return (get_reverse(x));
+    }
+};
